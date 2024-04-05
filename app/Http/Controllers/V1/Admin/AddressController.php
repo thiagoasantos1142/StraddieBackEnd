25a73ef4506c521275475eac5da4fa0ef14cac13
anderson <andersong.salvador@gmail.com>
2024-04-05 16:45:26 -0300

    att app

---
+++ app/Http/Controllers/V1/Admin/AddressController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //criar um novo endereço.
+        $validator = Validator::make($request->all(), [
+            'zip' => 'required|string|min:8|max:9',
+            'street_number' => 'required|string|max:10',
+            'neighborhood' => 'required|string',
+            'street' => 'required|string',
+            'city_id' => 'required|string',
+            'complent' => 'string|max:100'
+        ]);
+
+        if ($request->ajax()) {
+            // A requisição é AJAX
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 422);
+            }
+        }
+
+
+        //se não for requisição ajax ajustar o retorno aqui.
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
