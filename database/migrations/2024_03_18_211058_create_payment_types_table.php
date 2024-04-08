a6d438faae1fc0fb1ed3efd45bf201ea36dd1d80
Thiago <thiago.silva@clin.com.br>
2024-04-08 15:06:35 -0300

    ajustes hoem dashboard

--- database/migrations/2024_03_18_211058_create_payment_types_table.php
+++ database/migrations/2024_03_18_211058_create_payment_types_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('payment_types', function (Blueprint $table) {
             $table->id();
             $table->string('title', 255);
+            $table->string('description', 255);
             $table->timestamps();
         });
     }
