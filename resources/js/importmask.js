da6cef73bb8b273a9aeba31bfb0c04c845c90dcb
anderson <andersong.salvador@gmail.com>
2024-04-19 16:46:23 -0300

    att app

--- resources/js/importmask.js
+++ resources/js/importmask.js
@@ -1,2 +1,9 @@
+import jQuery from 'jquery';
+//config jquery
+window.jQuery = window.$ = jQuery;
+window.require = name => {
+  if (name === 'jquery') return window.jQuery || window.Zepto
+  else throw new Error(`Cannot require ${name}`)
+}
 import '~resources/js/jquery.mask.js';
 import '~resources/js/basicMask.js';
