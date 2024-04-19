a04372d919ff9c114f61963f01b97ef0fccff8c8
anderson <andersong.salvador@gmail.com>
2024-04-19 19:08:58 -0300

    att app

--- resources/views/layouts/components/scripts.blade.php
+++ resources/views/layouts/components/scripts.blade.php
@@ -1,28 +1,31 @@
+<!-- Scroll To Top -->
+<div class="scrollToTop">
+    <span class="arrow"><i class="fa fa-angle-up fs-20"></i></span>
+</div>
+<!-- Scroll To Top -->
 
-        <!-- Scroll To Top -->
-        <div class="scrollToTop">
-           <span class="arrow"><i class="fa fa-angle-up fs-20"></i></span>
-        </div>
-        <!-- Scroll To Top -->
+<div id="responsive-overlay"></div>
 
-        <div id="responsive-overlay"></div>
+<!-- Popper JS -->
+<script src="{{ asset('build/assets/libs/@popperjs/core/umd/popper.min.js') }}"></script>
 
-        <!-- Popper JS -->
-        <script src="{{asset('build/assets/libs/@popperjs/core/umd/popper.min.js')}}"></script>
+<!-- Bootstrap JS -->
+<script src="{{ asset('build/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-        <!-- Bootstrap JS -->
-        <script src="{{asset('build/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- Node Waves JS-->
+<script src="{{ asset('build/assets/libs/node-waves/waves.min.js') }}"></script>
 
-        <!-- Node Waves JS-->
-        <script src="{{asset('build/assets/libs/node-waves/waves.min.js')}}"></script>
+<!-- Simplebar JS -->
+<script src="{{ asset('build/assets/libs/simplebar/simplebar.min.js') }}"></script>
+@vite('resources/assets/js/simplebar.js')
 
-        <!-- Simplebar JS -->
-        <script src="{{asset('build/assets/libs/simplebar/simplebar.min.js')}}"></script>
-        @vite('resources/assets/js/simplebar.js')
+<!-- Color Picker JS -->
+<script src="{{ asset('build/assets/libs/@simonwep/pickr/pickr.es5.min.js') }}"></script>
 
-        <!-- Color Picker JS -->
-        <script src="{{asset('build/assets/libs/@simonwep/pickr/pickr.es5.min.js')}}"></script>
+<script src="{{ asset('build/assets/libs/awesome-notifications/index.var.js') }}"></script>
+<script>
+    var options = {}
+    var notifier = new AWN(options);
+</script>
 
-        <script src="{{ asset('build/assets/libs/awesome-notifications/index.var.js') }}"></script>
-
-        @yield('scripts')
+@yield('scripts')
