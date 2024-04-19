0f926b4e320968d9f640ffa66940546b30a9f2bd
anderson <andersong.salvador@gmail.com>
2024-04-19 18:46:53 -0300

    att app

--- resources/views/layouts/components/scripts.blade.php
+++ resources/views/layouts/components/scripts.blade.php
@@ -7,6 +7,8 @@
 
         <div id="responsive-overlay"></div>
 
+        <script src="{{ asset('build/assets/libs/awesome-notifications/index.var.js') }}"></script>
+
         <!-- Popper JS -->
         <script src="{{asset('build/assets/libs/@popperjs/core/umd/popper.min.js')}}"></script>
 
