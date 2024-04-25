189ce4115aba9d1e879eec25631c58d4195a0492
Thiago <thiago.silva@clin.com.br>
2024-04-25 15:39:23 -0300

    add campo crt_type_id em creditRightsTitle

---
+++ database/migrations/2024_04_25_183542_add_crt_type_id_to_crt_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('credit_rights_titles', function (Blueprint $table) {
+           
+            $table->foreignId('crt_type_id')->nullable()->constrained('crt_types');
+            
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('credit_rights_titles', function (Blueprint $table) {
+            $table->dropForeign(['crt_type_id']);
+            $table->dropColumn('crt_type_id');
+        });
+    }
+};
