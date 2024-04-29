52136ff8db0e4c1df48d60694f524a342dbf6172
anderson <andersong.salvador@gmail.com>
2024-04-29 15:28:38 -0300

    att app

--- resources/views/V1/Admin/creditRightsTitle/create.blade.php
+++ resources/views/V1/Admin/creditRightsTitle/create.blade.php
@@ -3,14 +3,9 @@
 @section('styles')
     <!-- noui Slider -->
     <link rel="stylesheet" href="{{ asset('build/assets/libs/nouislider/nouislider.min.css') }}">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
     <!-- Inclua os arquivos CSS e JavaScript do Select2 no seu layout -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 @endsection
 
 @section('content')
