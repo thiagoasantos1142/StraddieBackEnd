8bafc1ef60c5172f32c2be5154734fbec325d85f
anderson <andersong.salvador@gmail.com>
2024-04-30 11:24:21 -0300

    aatt app

--- resources/views/v1/admin/creditRightsTitle/show.blade.php
+++ resources/views/v1/admin/creditRightsTitle/show.blade.php
@@ -138,7 +138,7 @@
             {{-- @aqui --}}
             {{-- //a base de dados deve ser lawer --}}
             <x-v1.admin.modal.addCorporateUser :data-component="['credit_rights_title_id' => $creditRightsTitle->id]" :show-data="$lawyers" :search-dataroute="route('lawyer.index')"
-                :routeUpdate="route('crtLawyer.store')" ::title-card="Adicionar um advogado" :routeDelete="route('deleteLawyerCreditRigtsTitle')" ::action-btnremove="delete"></x-v1.admin.modal.addCorporateUser>
+                :routeUpdate="route('crtLawyer.store')" ::title-card="Atribuir um advogado" :routeDelete="route('deleteLawyerCreditRigtsTitle')" ::action-btnremove="delete"></x-v1.admin.modal.addCorporateUser>
 
 
         </div>
