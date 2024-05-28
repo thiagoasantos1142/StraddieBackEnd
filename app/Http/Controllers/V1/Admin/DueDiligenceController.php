c90acfcf70f002e36bdceb8ff47bb24bee3a9296
Thiago <thiago.silva@clin.com.br>
2024-05-28 13:47:58 -0300

    correção access dueDiligence

--- app/Http/Controllers/V1/Admin/DueDiligenceController.php
+++ app/Http/Controllers/V1/Admin/DueDiligenceController.php
@@ -24,7 +24,7 @@ class DueDiligenceController extends Controller
         $loggedUser = auth()->user();         
       
         // Verificar se o usuário tem a permissão para visualizar outros usuários
-        if (!Gate::allows('view-dueDiligences', auth()) && $loggedUser->user_type_id != 1) {
+        if (!Gate::allows('view-dueDiligences', $loggedUser) || !Gate::allows('access-admin', $loggedUser) {
             // Se não tiver permissão, lance uma exceção de autorização
             abort(403, 'Você não tem permissão para visualizar Due Dilogence.');
         }
