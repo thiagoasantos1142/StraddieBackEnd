6b2b319f2e1b9811b9e9868549541684b6cf93c8
Thiago <thiago.silva@clin.com.br>
2024-08-27 11:47:49 -0300

    c

--- resources/views/v1/admin/assets/show.blade.php
+++ resources/views/v1/admin/assets/show.blade.php
@@ -254,7 +254,7 @@
     <!-- Offer Modal -->
     <div class="modal fade" id="offerModal" tabindex="-1" aria-labelledby="offerModalLabel" aria-hidden="true">
         <div class="modal-dialog">
-            <form method="POST" action="{{ route('assets.makeOffer') }}" id="offerForm">
+            <form method="POST" action="{{ route('assets.makeOffer', ['assetId' => $availableAsset->id]) }}" enctype="multipart/form-data">
                 @csrf
                 <div class="modal-content">
                     <div class="modal-header">
