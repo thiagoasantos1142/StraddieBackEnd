be4c85e7c920aa2e435593e02cdc1768648b409d
Thiago <thiago.silva@clin.com.br>
2024-08-30 11:05:00 -0300

    c

--- app/Http/Controllers/V1/Admin/OfferController.php
+++ app/Http/Controllers/V1/Admin/OfferController.php
@@ -42,22 +42,11 @@ class OfferController extends Controller
         
         if ($request->ajax()) {
             $offers = Offer::query()
-                            ->with('asset.due_diligence.crt.users_titles', 'status', 'organization', 'user', 'category')
-                            ->where(function ($query) use ($loggedUser) {
-                                $query->whereNotNull('organization_id')
-                                    ->where('organization_id', $loggedUser->organization_id);
-                            })
-                            ->orWhere(function ($query) use ($loggedUser) {
-                                $query->whereNotNull('offers.created_by')
-                                    ->where('offers.created_by', $loggedUser->id);
-                            })
-                            ->orWhere(function ($query) use ($loggedUser) {
-                                $query->whereNotNull('offers.user_id')
-                                    ->where('offers.user_id', $loggedUser->id);
-                            })
-                            ->orderBy('offers.created_at', 'desc')
-                            ->get();
-        
+                ->with('asset.due_diligence.crt.users_titles', 'status', 'organization', 'user', 'category')             
+                ->orWhere('organization_id', $loggedUser->organization_id)
+                ->orWhere('offers.created_by', $loggedUser->id)
+                ->orWhere('offers.user_id', $loggedUser->id)
+                ->orderBy('offers.created_at', 'desc');
             
             $table = DataTables::of($offers);
             
